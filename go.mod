6feb4a7ad5bcbe36b1bdba934e06d4b9ebedd42e
mzz2017 <m@mzz.pub>
2020-11-28 21:57:22 +0800

    support udp

---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/cumtyc/dns2socks5
+
+go 1.15
+
+require (
+	github.com/patrickmn/go-cache v2.1.0+incompatible // indirect
+	github.com/txthinking/runnergroup v0.0.0-20200327135940-540a793bb997 // indirect
+	github.com/txthinking/socks5 v0.0.0-20201123045853-9265fe887898
+	github.com/txthinking/x v0.0.0-20200813135806-2c9a7191ffb3 // indirect
+	golang.org/x/net v0.0.0-20201110031124-69a78807bb2b
+)
--- main.go
+++ main.go
@@ -1,67 +1,83 @@
 package main
 
 import (
-	"golang.org/x/net/proxy"
-	"fmt"
-	"os"
-	"net"
 	"encoding/binary"
 	"flag"
+	"fmt"
+	"github.com/txthinking/socks5"
+	"golang.org/x/net/proxy"
+	"net"
+	"os"
 )
 
-
-func main(){
+func main() {
 	var flags struct {
 		Bind        string
 		SocksServer string
-		DNSServer	string
+		SocksUDP    bool
+		DNSServer   string
 	}
 	flag.StringVar(&flags.Bind, "bind", "127.0.0.1:8053", "address to bind")
 	flag.StringVar(&flags.SocksServer, "socks-server", "127.0.0.1:1080", "socks5 server address")
+	flag.BoolVar(&flags.SocksUDP, "socks-udp", false, "dial socks5 using udp")
 	flag.StringVar(&flags.DNSServer, "dns-server", "8.8.8.8:53", "remote dns server")
 	flag.Parse()
-	dialer, _ := proxy.SOCKS5("tcp", flags.SocksServer, nil, proxy.Direct)
+	network := "tcp"
+	if flags.SocksUDP {
+		network = "udp"
+	}
+	dialer, _ := socks5.NewClient(flags.SocksServer, "", "", 30, 30)
 	localConn, err := net.ListenPacket("udp", flags.Bind)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "can't bind:", flags.Bind, err)
 		os.Exit(1)
 	}
-	for{
+	for {
 		buf := make([]byte, 64*1024)
-		n, addr, _ := localConn.ReadFrom(buf[2:])
-		go handleQuery(flags.DNSServer, dialer, localConn, n, addr, buf)
+		n, addr, _ := localConn.ReadFrom(buf)
+		go handleQuery(flags.DNSServer, dialer, network, localConn, n, addr, buf[:n])
 	}
 }
 
-
-func handleQuery(dnsServer string, dialer proxy.Dialer, localConn net.PacketConn, n int, addr net.Addr, buf []byte){
-	conn, err := dialer.Dial("tcp", dnsServer)
+func handleQuery(dnsServer string, dialer proxy.Dialer, network string, localConn net.PacketConn, n int, addr net.Addr, buf []byte) {
+	conn, err := dialer.Dial(network, dnsServer)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "connect dns server error:", err)
 		return
 	}
 	defer conn.Close()
-	binary.BigEndian.PutUint16(buf, uint16(n))
-	_, err = conn.Write(buf[:n+2])
+	lenBuf := make([]byte, 2)
+	if network == "tcp" {
+		buf = append(buf, lenBuf...) // just extend the length
+		binary.BigEndian.PutUint16(lenBuf, uint16(n))
+		copy(buf[2:], buf)
+		copy(buf, lenBuf)
+	}
+	_, err = conn.Write(buf)
 	if err != nil {
-		fmt.Fprintln(os.Stderr,"write to dns server error:", err)
+		fmt.Fprintln(os.Stderr, "write to dns server error:", err)
 		return
 	}
-	n, err = conn.Read(buf[:2])
+	var size uint16
+	if network == "tcp" {
+		n, err = conn.Read(lenBuf)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "read length from dns server error:", err)
+			return
+		}
+		size = binary.BigEndian.Uint16(lenBuf)
+	}
+	n, err = conn.Read(buf)
 	if err != nil {
-		fmt.Fprintln(os.Stderr,"read length from dns server error:", err)
+		fmt.Fprintln(os.Stderr, "read data from dns server error:", err)
 		return
 	}
-	size := binary.BigEndian.Uint16(buf[:2])
-	resBuf := buf[2:2+size]
-	n, err = conn.Read(resBuf)
-	if err != nil {
-		fmt.Fprintln(os.Stderr,"read data from dns server error:", err)
-		return
+	if size != 0 && n > int(size) {
+		n = int(size)
 	}
-	n, err = localConn.WriteTo(resBuf, addr)
+	n, err = localConn.WriteTo(buf[:n], addr)
 	if err != nil {
-		fmt.Fprintln(os.Stderr,"back write error %s", err.Error())
+		fmt.Fprintln(os.Stderr, "back write error:", err.Error())
 		return
 	}
 }
